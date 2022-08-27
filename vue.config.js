6f79c432761ea33e3f084d08a953dec7f2bacd09
Peach <2990221345@qq.com>
2022-08-27 23:29:25 +0800

    build

--- vue.config.js
+++ vue.config.js
@@ -3,23 +3,9 @@ module.exports = {
   publicPath: "./",
   outputDir: "docs",
   productionSourceMap: false,
-  devServer: {
-    proxy: {
-      "^/api": {
-        target: process.env.VUE_APP_BASE_URL,
-        changeOrigin: true,
-        pathRewrite: {
-          "^/api": "",
-        },
-      },
-    },
-  },
-  // 配置方式二：和webpack属性完全一致，最后会进行合并
-  configureWebpack: {
-    resolve: {
-      alias: {
-        components: "@/components",
-      },
-    },
+  chainWebpack: (config) => {
+    config.resolve.alias
+      .set("@", path.resolve(__dirname, "src"))
+      .set("components", path.resolve(__dirname, "@/components"));
   },
 };
